dbbca7bf9a39f4d87c3d8405b173cfd9e6e534f4
Taylor <258266864@qq.com>
2019-03-29 16:52:13 +0800

    add

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -2,9 +2,11 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy  # new
+from flask_debugtoolbar import DebugToolbarExtension
 
 # instantiate the db
-db = SQLAlchemy()  # new
+db = SQLAlchemy()
+toolbar = DebugToolbarExtension()
 
 
 def create_app(script_info=None):
@@ -16,6 +18,7 @@ def create_app(script_info=None):
     app.config.from_object(app_settings)
 
     db.init_app(app)
+    toolbar.init_app(app)
 
     from project.api.users import users_blueprint
     app.register_blueprint(users_blueprint)
