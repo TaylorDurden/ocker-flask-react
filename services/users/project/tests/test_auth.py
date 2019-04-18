25cb12305f071e57bd418bc3c35a20ee5da62885
Taylor <258266864@qq.com>
2019-04-18 09:01:15 +0800

    test

--- services/users/project/tests/test_auth.py
+++ services/users/project/tests/test_auth.py
@@ -9,6 +9,8 @@ from project.api.models import User
 from project.tests.base import BaseTestCase
 import json
 
+JSON_CONTENT_TYPE='application/json'
+
 
 class TestAuthBlueprint(BaseTestCase):
 
@@ -28,8 +30,70 @@ class TestAuthBlueprint(BaseTestCase):
             self.assertTrue(data['status'] == 'success')
             self.assertTrue(data['message'] == '注册成功.')
             self.assertTrue(data['auth_token'])
-            self.assertTrue(response.content_type == 'application/json')
+            self.assertTrue(response.content_type == JSON_CONTENT_TYPE)
             self.assertEqual(response.status_code, 201)
+
+    def test_registered_with_already_registered_user(self):
+        """ Test registration with already registered email """
+        user = User(
+            username='123456',
+            email='joe@gmail.com',
+        )
+        db.session.add(user)
+        db.session.commit()
+        with self.client:
+            response = self.client.post(
+                '/auth/register',
+                data=json.dumps(dict(
+                    username='123456',
+                    email='joe@gmail.com',
+                )),
+                content_type='application/json'
+            )
+            data = json.loads(response.data.decode())
+            self.assertTrue(data['status'] == 'fail')
+            self.assertTrue(
+                data['message'] == '用户已存在，请登录.'
+            )
+            self.assertTrue(response.content_type == JSON_CONTENT_TYPE)
+            self.assertEqual(response.status_code, 202)
+
+    def test_registered_user_login(self):
+        """ Test for login of registered-user login """
+        with self.client:
+            # user registration
+            response_register = self.client.post(
+                '/auth/register',
+                data=json.dumps(dict(
+                    email='joe@gmail.com',
+                    password='123456'
+                )),
+                content_type=JSON_CONTENT_TYPE,
+            )
+            data_register = json.loads(response_register.data.decode())
+            self.assertTrue(data_register['status'] == 'success')
+            self.assertTrue(
+                data_register['message'] == '注册成功.'
+            )
+            self.assertTrue(data_register['auth_token'])
+            self.assertTrue(response_register.content_type == JSON_CONTENT_TYPE)
+            self.assertEqual(response_register.status_code, 201)
+            # registered user login
+            response = self.client.post(
+                '/auth/login',
+                data=json.dumps(dict(
+                    username='123456',
+                    email='joe@gmail.com',
+                    password='123456'
+                )),
+                content_type='application/json'
+            )
+            data = json.loads(response.data.decode())
+            self.assertTrue(data['status'] == 'success')
+            self.assertTrue(data['message'] == '登录成功.')
+            self.assertTrue(data['auth_token'])
+            self.assertTrue(response.content_type == JSON_CONTENT_TYPE)
+            self.assertEqual(response.status_code, 200)
 
 
 if __name__ == '__main__':
