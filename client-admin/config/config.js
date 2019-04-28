42c7d7a3a8fc00f119bca7a10c4d991f623acdec
taylorlee <258266864@qq.com>
2019-04-28 15:29:17 +0800

    local dev

--- client-admin/config/config.js
+++ client-admin/config/config.js
@@ -77,13 +77,13 @@ export default {
   theme: {
     'primary-color': primaryColor,
   },
-  // proxy: {
-  //   '/server/api/': {
-  //     target: 'https://preview.pro.ant.design/',
-  //     changeOrigin: true,
-  //     pathRewrite: { '^/server': '' },
-  //   },
-  // },
+  proxy: {
+    '/api/': {
+      target: 'http://localhost:5000/',
+      changeOrigin: true,
+      pathRewrite: { '^/api': '' },
+    },
+  },
   ignoreMomentLocale: true,
   lessLoaderOptions: {
     javascriptEnabled: true,
