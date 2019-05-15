239b6c4948feb804f71e2fea4f395e15c10548a1
Taylor <258266864@qq.com>
2019-05-16 00:19:01 +0800

    fix migrate

--- services/users/project/api/models.py
+++ services/users/project/api/models.py
@@ -60,7 +60,7 @@ class User(PaginatedAPIMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     phone = db.Column(db.String(120), index=True, unique=True)
     posts = db.relationship('Post', backref='author', lazy='dynamic')
-    avatar = db.Column(db.String(256))
+    avatar = db.Column(db.String())
     active = db.Column(db.Boolean(), default=True, nullable=False)
     followed = db.relationship(
         'User', secondary=followers,
