5f043a27bdcce8550f9db62f3f641d43a8970017
taylorlee <258266864@qq.com>
2019-06-13 19:14:24 +0800

    fix

--- services/users/project/api/models/article.py
+++ services/users/project/api/models/article.py
@@ -7,13 +7,34 @@ from project.config import BaseConfig
 import jwt
 from flask import url_for
 
+article_tags = db.Table('article_tags',
+                     db.Column('follower_id',
+                               db.Integer,
+                               db.ForeignKey('user.id')),
+                     db.Column('followed_id',
+                               db.Integer,
+                               db.ForeignKey('user.id')),
+                     )
+
 
 class Article(db.Model):
     __tablename__ = 'article'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String(128), nullable=False)
-    body = db.Column(db.Text())
-
+    body = db.Column(db.Text(), nullable=False)
+    author_name = db.Column(db.String(128))
+    tags = db.relationship('tag', backref='articles', lazy='dynamic')
+    open_comment = db.Column(db.Boolean(), default=True, nullable=False)
+    created_date = db.Column(db.DateTime, default=func.now(), nullable=False)
+    last_edit_date = db.Column(db.DateTime, default=func.now(), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    like_count = db.Column(db.Integer)
+    fav_count = db.Column(db.Integer)
+    visit_count = db.Column(db.Integer)
+    comments = db.relationship('comment', backref='article', lazy='dynamic')
+    tags = db.relationship(
+        'tag', secondary=article_tags,
+        backref=db.backref('articles', lazy='dynamic'), lazy='dynamic')
     pass
 
 
@@ -24,9 +45,21 @@ class Tag(db.Model):
     pass
 
 
+class ArticleTags(db.Model):
+    __tablename__ = "article_tags"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+
+
 class Category(db.Model):
+    __tablename__ = 'category'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(128), nullable=False)
     pass
 
 
 class Comment(db.Model):
+    __tablename__ = 'comment'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    content = db.Column(db.String(4000), nullable=False)
+
     pass
